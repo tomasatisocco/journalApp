0e5071097f131444ce5e87d2dee68a73adf7f502
Tomas Tisocco <tomastisocco97@gmail.com>
2021-08-26 00:05:17 -0300

    Create Home_bloc_provider.dart

---
+++ lib/blocs/home_bloc_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:journal/blocs/home_bloc.dart';
+
+class HomeBlocProvider extends InheritedWidget{
+  final HomeBloc homebloc;
+  final String uid;
+
+  HomeBlocProvider({required Key key, required Widget child, required this.homebloc, required this.uid})
+  : super(key: key, child: child);
+
+  static HomeBlocProvider of(BuildContext context){
+    return (context.dependOnInheritedWidgetOfExactType() as HomeBlocProvider);
+  }
+
+  @override
+  bool updateShouldNotify(HomeBlocProvider old) =>
+    homebloc != old.homebloc;
+}
